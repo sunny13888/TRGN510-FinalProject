8fe8efb0bfc6a138c213daa47aa8b34ae5b25557
sunny13888 <35489016+sunny13888@users.noreply.github.com>
2018-04-29 12:03:24 -0700

    Add files via upload

---
+++ heatmap.R
@@ -0,0 +1,64 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+# load data
+setwd("~/Desktop/510final")
+load("finaldata.RData")
+
+# load libraries
+library(shiny)
+library(ggplot2)
+library(reshape2)
+library('plotly')
+library("dplyr")
+library(ComplexHeatmap)
+library(circlize)
+library(colorspace)
+library(GetoptLong)
+library(MASS)
+library(pvclust)
+
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Gene expression"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+      sidebarPanel(
+         colourInput("col",
+                     "Select colour", "blue"),
+         colourInput("col2",
+                     "Select colour", "purple")
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput("distPlot")
+      )
+   )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+   
+   output$distPlot <- renderPlot({
+      # generate bins based on input$bins from ui.R
+    
+      
+      # heatmap
+     Heatmap(finaldataAW2.log.small, name = "expression", km = 5, top_annotation = ha, top_annotation_height = unit(4, "mm"), show_row_names = FALSE, show_column_names = FALSE,col = colorRamp2(c(0, 20), c(input$col, input$col2)))
+   })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
